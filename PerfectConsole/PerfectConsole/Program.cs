950386f3f3ecd87ec0615198ba9e709861f67941
Dmitry Poluektov <dpoluektov@gmail.com>
2022-10-05 00:43:50 +0300

    DependencyInjection and Hosting

--- PerfectConsole/PerfectConsole/Program.cs
+++ PerfectConsole/PerfectConsole/Program.cs
@@ -1,8 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-// IAmTimCorey
-// https://www.youtube.com/watch?v=dZSLm4tOI8o&ab_channel=IAmTimCorey
-/*
- * There are certain best practices every C# application should follow, right? Probably DRY, SOLID, Logging, Dependency Injection, Localization, Separation of Concerns, and Unit Testing at the very least. These seem like obvious things that every application should have. In this video, we are going to do something a bit unique. I am going to show you how to do all of those things, and I am going to show you why you shouldn't always do those things. We are going to build the ultimate Hello World application. You won't want to miss it.
- */
+﻿using PerfectConsole;
+using PerfectConsoleLibrary.BusinessLogic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    services.GetRequiredService<App>().Run(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args).
+    ConfigureServices((_, services) =>
+    {
+        services
+            .AddSingleton<App>()
+            .AddSingleton<IMessages, Messages>();
+        
+    });
