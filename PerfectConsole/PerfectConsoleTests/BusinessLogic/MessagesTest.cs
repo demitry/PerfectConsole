658337dd18309fb1b32982a15bca3d115670da1d
Dmitry Poluektov <dpoluektov@gmail.com>
2022-10-05 01:29:17 +0300

    xUnit tests

---
+++ PerfectConsole/PerfectConsoleTests/BusinessLogic/MessagesTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using PerfectConsoleLibrary.BusinessLogic;
+
+namespace PerfectConsoleTests.BusinessLogic
+{
+    public class MessagesTest
+    {
+        [Fact]
+        public void Greeting_InEnglish()
+        {
+            ILogger<Messages> logger = new NullLogger<Messages>();
+            Messages messages = new(logger);
+
+            string expected = "Hello World!";
+            string actual = messages.Greeting("en");
+            
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Greeting_InSpanish()
+        {
+            ILogger<Messages> logger = new NullLogger<Messages>();
+            Messages messages = new(logger);
+
+            string expected = "Hola Mundo!";
+            string actual = messages.Greeting("es");
+            
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Greeting_InFrench()
+        {
+            ILogger<Messages> logger = new NullLogger<Messages>();
+            Messages messages = new(logger);
+
+            string expected = "Salut tout le monde!";
+            string actual = messages.Greeting("fr");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Greeting_Invalid()
+        {
+            ILogger<Messages> logger = new NullLogger<Messages>();
+            Messages messages = new(logger);
+
+            Assert.Throws<InvalidOperationException>(() => messages.Greeting("xx"));
+        }
+    }
+}
